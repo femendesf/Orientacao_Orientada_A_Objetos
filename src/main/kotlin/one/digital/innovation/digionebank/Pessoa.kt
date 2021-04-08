a3a8aa94e715789b1d1b735280602c466c7aa0b8
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2021-04-08 20:09:24 -0300

    Update class Pessoa

--- src/main/kotlin/one/digital/innovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digital/innovation/digionebank/Pessoa.kt
@@ -2,11 +2,19 @@ package one.digital.innovation.digionebank
 
 class Pessoa {
     var name:String = "Felipe"
+
     var cpf: String = "123.456.789-10"
+
+
+    inner class Endereco( // Classe interna da classe Pessoa
+        var rua: String = "Rua Radialista Souza Moreno"
+    )
 }
 fun main() {
     val cliente = Pessoa()
 
+    cliente.cpf = "1234"
     println(cliente.name)
     println(cliente.cpf)
+    println(cliente.Endereco().rua)
 }
