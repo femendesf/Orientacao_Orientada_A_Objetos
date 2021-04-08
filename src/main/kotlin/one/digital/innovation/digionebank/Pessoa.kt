6429492f127494f4aea5683412e93c6d997a6e68
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2021-04-08 20:23:24 -0300

    Adicionado contructor

--- src/main/kotlin/one/digital/innovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digital/innovation/digionebank/Pessoa.kt
@@ -4,17 +4,26 @@ class Pessoa {
     var name:String = "Felipe"
 
     var cpf: String = "123.456.789-10"
+    private set // Privando apenas o set da variavel cpf
+    // 'name' e 'cpf' são os membros das classes
+
 
 
     inner class Endereco( // Classe interna da classe Pessoa
         var rua: String = "Rua Radialista Souza Moreno"
     )
+
+    constructor()
+    fun pessoaInfo() = "$name e $cpf" // Função permite objetos terem comportamentos
 }
 fun main() {
     val cliente = Pessoa()
 
-    cliente.cpf = "1234"
+
     println(cliente.name)
     println(cliente.cpf)
     println(cliente.Endereco().rua)
+    println("*******************************")
+    println(cliente.pessoaInfo())
+
 }
